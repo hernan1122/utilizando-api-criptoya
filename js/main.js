01f72a1edee5afe0ef05ec673761896d8e1b57b0
Axel Torrico <torricohernan2@gmail.com>
2022-10-24 13:59:31 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+async function dolarOficial() {
+  const res = await fetch('https://criptoya.com/api/dolar');
+  const data = await res.json();
+  console.log(data);
+
+  const oficial = document.getElementById('oficial');
+  const solidario = document.getElementById('solidario');
+  const blue = document.getElementById('blue');
+  const cripto = document.getElementById('cripto');
+
+  oficial.innerHTML = data.oficial;
+  solidario.innerHTML = data.solidario;
+  blue.innerHTML = data.blue;
+  cripto.innerHTML = data.ccb;
+}
+
+dolarOficial();
