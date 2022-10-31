0471f7c7b7f2aad090ec9334c664090919664643
Axel Torrico <torricohernan2@gmail.com>
2022-10-31 17:19:36 -0300

    agregue mas cripto

--- js/main.js
+++ js/main.js
@@ -19,6 +19,22 @@ async function consultaCripto() {
   const ripioVenta = document.getElementById('ripio-venta');
   const budaCompra = document.getElementById('buda-compra');
   const budaVenta = document.getElementById('buda-venta');
+  const deCompra = document.getElementById('de-compra');
+  const deVenta = document.getElementById('de-venta');
+  const copterCompra = document.getElementById('copter-compra');
+  const copterVenta = document.getElementById('copter-venta');
+  const saldoCompra = document.getElementById('saldo-compra');
+  const saldoVenta = document.getElementById('saldo-venta');
+  const ftxCompra = document.getElementById('ftx-compra');
+  const ftxVenta = document.getElementById('ftx-venta');
+  const beloCompra = document.getElementById('belo-compra');
+  const beloVenta = document.getElementById('belo-venta');
+  const calypsoCompra = document.getElementById('calypso-compra');
+  const calypsoVenta = document.getElementById('calypso-venta');
+  const fiwindCompra = document.getElementById('fiwind-compra');
+  const fiwindVenta = document.getElementById('fiwind-venta');
+  const bitexCompra = document.getElementById('bitex-compra');
+  const bitexVenta = document.getElementById('bitex-venta');
 
   argenCompra.innerHTML = `$` + data.argenbtc.totalAsk;
   argenVenta.innerHTML = `$` + data.argenbtc.totalBid;
@@ -28,6 +44,22 @@ async function consultaCripto() {
   ripioVenta.innerHTML = `$` + data.ripio.totalBid;
   budaCompra.innerHTML = `$` + data.buda.totalAsk;
   budaVenta.innerHTML = `$` + data.buda.totalBid;
+  deCompra.innerHTML = `$` + data.decrypto.totalAsk;
+  deVenta.innerHTML = `$` + data.decrypto.totalBid;
+  copterCompra.innerHTML = `$` + data.copter.totalAsk;
+  copterVenta.innerHTML = `$` + data.copter.totalBid;
+  saldoCompra.innerHTML = `$` + data.saldo.totalAsk;
+  saldoVenta.innerHTML = `$` + data.saldo.totalBid;
+  ftxCompra.innerHTML = `$` + data.ftx.totalAsk;
+  ftxVenta.innerHTML = `$` + data.ftx.totalBid;
+  beloCompra.innerHTML = `$` + data.belo.totalAsk;
+  beloVenta.innerHTML = `$` + data.belo.totalBid;
+  calypsoCompra.innerHTML = `$` + data.calypso.totalAsk;
+  calypsoVenta.innerHTML = `$` + data.calypso.totalBid;
+  fiwindCompra.innerHTML = `$` + data.fiwind.totalAsk;
+  fiwindVenta.innerHTML = `$` + data.fiwind.totalBid;
+  bitexCompra.innerHTML = `$` + data.bitex.totalAsk;
+  bitexVenta.innerHTML = `$` + data.bitex.totalBid;
 }
 
 async function dolarOficial() {
